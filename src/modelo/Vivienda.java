b4bd71035cfc4167a280571141ba3065dcf8b6e4
Dmytro <dim4ukt@gmail.com>
2026-02-19 11:16:10 +0100

    Clase Vivienda creada, atributos creados, enum Estado_VIVIENDA y Calidad_VIVIENDA creados

--- src/modelo/Vivienda.java
+++ src/modelo/Vivienda.java
@@ -1,4 +1,25 @@
 package modelo;
 
+enum Estado_VIVIENDA {
+    LIBRE,
+    RESERVADO,
+    VENDIDO
+}
+enum Calidad_VIVIENDA {
+    ESTANDAR,
+    PLUS,
+    DE_LUXE
+
+}
+
 public class Vivienda {
+
+     Estado_VIVIENDA estado = Estado_VIVIENDA.LIBRE;
+     double precio;
+     double metrosCuadrados;
+     int habitaciones;
+     String dniComprador = null;
+     Calidad_VIVIENDA calidad = null;
+
+
 }
