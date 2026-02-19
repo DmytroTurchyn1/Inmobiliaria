36a38f6c11b881c4012c2ebbeeac1a380429577f
navip <navi.pa.07@gmail.com>
2026-02-19 11:22:13 +0100

    He metido hasta 1/2 set y get de plaza garaje

--- src/modelo/PlazaGaraje.java
+++ src/modelo/PlazaGaraje.java
@@ -12,7 +12,28 @@ public class PlazaGaraje {
     double metrosCuadrados = 0;
     String dniComprador ;
     double UMBRAL_GRANDE = 12;  //revisar
+    Estado estado;
 
+    //contructor
+    public PlazaGaraje(double precio, double metrosCuadrados){
+        this.estado = Estado.LIBRE;
+        this.precio= precio;
+        this.metrosCuadrados = metrosCuadrados;
+        dniComprador = null;
+
+    }
+
+    //Getter y setter
+
+    public Estado getEstado(){
+        return estado;
+    }
+
+    public void setEstado(Estado estado){
+        this.estado = estado;
+    }
+
+    
 
 }
 
