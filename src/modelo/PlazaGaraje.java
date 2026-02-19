bb845d611a9576597ecddbe98ab953eb0f4e513b
navip <navi.pa.07@gmail.com>
2026-02-19 11:06:34 +0100

    proyect setup

--- src/modelo/PlazaGaraje.java
+++ src/modelo/PlazaGaraje.java
@@ -1,6 +1,19 @@
 package modelo;
 
 public class PlazaGaraje {
+
+    //Enumero el estado
+    public enum Estado{
+        LIBRE, VENDIDA;
+    }
+
+    //defino las variables
+    double precio = 0;
+    double metrosCuadrados = 0;
+    String dniComprador ;
+    double UMBRAL_GRANDE = 12;  //revisar
+
+
 }
 
 
