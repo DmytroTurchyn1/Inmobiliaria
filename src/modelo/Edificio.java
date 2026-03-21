5243ba70c2836e48b28b22d9771aee661c574733
navip <navi.pa.07@gmail.com>
2026-03-21 13:53:09 +0100

    PARTE 5 Y 6, INGRESOS Y CALCULOS TERMINADOS

--- src/modelo/Edificio.java
+++ src/modelo/Edificio.java
@@ -102,9 +102,258 @@ public class Edificio {
         }
         System.out.println();
 
+    }
+
+    public int getTotalViviendas(){
+        int contadorTotViviendas = 0;
+
+        for(int planta = numPlantas - 1; planta >= 0; planta--){
+            for (int puerta = 0; puerta < viviendasPorPlanta; puerta++){
+                Vivienda vivienda = viviendas[planta][puerta];
+
+                if(vivienda != null){
+                    contadorTotViviendas = contadorTotViviendas +1;
+
+                }
+
+            }
+        }
+
+        return contadorTotViviendas;
+    }
+
+    public int contarViviendasLibres(){
+        int contadorVivLibres = 0;
+
+        for (int planta = numPlantas - 1; planta >= 0; planta--){
+            for (int puerta = 0; puerta < viviendasPorPlanta; puerta++){
+                Vivienda vivienda = viviendas[planta][puerta];
+
+                if(vivienda != null && vivienda.estado == Vivienda.Estado.LIBRE ){
+                    contadorVivLibres = contadorVivLibres + 1;
+                }
+            }
+        }
+
+        return contadorVivLibres;
+    }
+
+    public int contarViviendasReservadas(){
+        int contadorVivReservadas = 0;
+
+        for (int planta = numPlantas - 1; planta >= 0; planta--){
+            for (int puerta = 0; puerta < viviendasPorPlanta; puerta++){
+                Vivienda vivienda = viviendas[planta][puerta];
+
+                if(vivienda != null && vivienda.estado == Vivienda.Estado.RESERVADO){
+                    contadorVivReservadas = contadorVivReservadas + 1;
+                }
+            }
+        }
+
+        return contadorVivReservadas;
+    }
+
+    public int contarViviendasVendidas(){
+        int contadorVivVendidas = 0;
+
+        for (int planta = numPlantas - 1; planta >= 0; planta--){
+            for (int puerta = 0; puerta < viviendasPorPlanta; puerta++){
+                Vivienda vivienda = viviendas[planta][puerta];
+
+                if(vivienda != null && vivienda.estado == Vivienda.Estado.VENDIDO){
+                    contadorVivVendidas = contadorVivVendidas + 1;
+                }
+            }
+        }
+
+        return contadorVivVendidas;
+    }
+
+    public int getTotalPlazasGaraje(){
+        int totalPlazasGaraje = 0;
+
+        for(int planta = 0; planta < 2; planta++){
+            for (int plazas = 0; plazas < plazasPorPlantaGaraje; plazas++){
+                PlazaGaraje plazaGaraje = garaje[planta][plazas];
+
+                if(plazaGaraje != null){
+                    totalPlazasGaraje = totalPlazasGaraje + 1;
+                }
+            }
+        }
+
+        return  totalPlazasGaraje;
+    }
+
+    public int contarPlazasGarajeVendidas(){
+        int plazasGarajeVendidas = 0;
+
+        for(int planta = 0; planta < 2; planta++){
+            for (int plazas = 0; plazas < plazasPorPlantaGaraje; plazas++){
+                PlazaGaraje plazaGaraje = garaje[planta][plazas];
+
+                if(plazaGaraje != null && plazaGaraje.estado == PlazaGaraje.Estado.VENDIDO){
+                    plazasGarajeVendidas = plazasGarajeVendidas + 1;
+                }
+            }
+        }
+
+        return  plazasGarajeVendidas;
+    }
+
+    public int contarPlazasGarajeLibres(){
+        int plazasGarajeLibres = 0;
+
+        for(int planta = 0; planta < 2; planta++){
+            for (int plazas = 0; plazas < plazasPorPlantaGaraje; plazas++){
+                PlazaGaraje plazaGaraje = garaje[planta][plazas];
+
+                if(plazaGaraje != null && plazaGaraje.estado == PlazaGaraje.Estado.LIBRE){
+                    plazasGarajeLibres = plazasGarajeLibres + 1;
+                }
+            }
+        }
+
+        return plazasGarajeLibres;
+    }
 
 
+    public int getTotalTrasteros(){
+        int numTotalTrasteros = 0;
+
+        for(int puerta = 0; puerta < numTrasteros; puerta++){
+            Trastero trastero = trasteros[puerta];
+            if(trastero != null){
+                numTotalTrasteros = numTotalTrasteros + 1;
+            }
+        }
+
+        return numTotalTrasteros;
+    }
+
+    public int totalTrasterosVendidos(){
+        int trasterosVendidos = 0;
+
+        for(int puerta = 0;  puerta < numTrasteros ; puerta++){
+            Trastero trastero = trasteros[puerta];
+            if(trastero != null && trastero.estado == Trastero.Estado.VENDIDO ){
+                trasterosVendidos = trasterosVendidos + 1;
+            }
+        }
+
+        return trasterosVendidos;
+    }
+
+    public int totalTrasterosLibres(){
+        int trasterosLibres = 0;
+
+        for(int puerta = 0;  puerta < numTrasteros ; puerta++){
+            Trastero trastero = trasteros[puerta];
+            if(trastero != null && trastero.estado == Trastero.Estado.LIBRE ){
+                trasterosLibres = trasterosLibres + 1;
+            }
+        }
+
+        return trasterosLibres;
+    }
+
+
+    //CALCULOS
+
+    public double calculoIngresoPotencialesViviendas(){
+        double ingresoPotencial = 0;
+
+        for(int planta = numPlantas - 1; planta >= 0; planta--){
+            for (int puerta = 0; puerta < viviendasPorPlanta; puerta++){
+                Vivienda vivienda = viviendas[planta][puerta];
+
+                if(vivienda != null){
+                    ingresoPotencial = ingresoPotencial + vivienda.getPrecio();
+                }
+
+            }
+        }
+
+        return ingresoPotencial;
 
     }
 
+    public double calculoIngresosVivVendidas(){
+        double ingresoVivVendidas = 0;
+
+        for (int planta = numPlantas - 1; planta >= 0; planta--){
+            for (int puerta = 0; puerta < viviendasPorPlanta; puerta++){
+                Vivienda vivienda = viviendas[planta][puerta];
+
+                if(vivienda != null && vivienda.estado == Vivienda.Estado.VENDIDO){
+                    ingresoVivVendidas = ingresoVivVendidas + vivienda.getPrecio();
+                }
+            }
+        }
+
+        return ingresoVivVendidas;
+    }
+
+    public double calculoIngresoPotencialPlazaGaraje(){
+        double ingresoPotencialPlazGaraje = 0;
+
+        for(int planta = 0; planta < 2; planta++){
+            for (int plazas = 0; plazas < plazasPorPlantaGaraje; plazas++){
+                PlazaGaraje plazaGaraje = garaje[planta][plazas];
+
+                if(plazaGaraje != null){
+                    ingresoPotencialPlazGaraje = ingresoPotencialPlazGaraje + plazaGaraje.getPrecio();
+                }
+            }
+        }
+
+        return ingresoPotencialPlazGaraje;
+    }
+
+    public double calculoIngresosVendidosPlazGaraj(){
+        double ingresoVendidosPLazas = 0;
+
+        for(int planta = 0; planta < 2; planta++){
+            for (int plazas = 0; plazas < plazasPorPlantaGaraje; plazas++){
+                PlazaGaraje plazaGaraje = garaje[planta][plazas];
+
+                if(plazaGaraje != null && plazaGaraje.estado == PlazaGaraje.Estado.VENDIDO){
+                    ingresoVendidosPLazas = ingresoVendidosPLazas + plazaGaraje.getPrecio();
+                }
+            }
+        }
+
+        return ingresoVendidosPLazas;
+    }
+
+    public double calculoIngresoPotencialesTrasteros(){
+        double ingresoPotencialesTrasteros = 0;
+
+        for(int puerta = 0; puerta < numTrasteros; puerta++){
+            Trastero trastero = trasteros[puerta];
+            if(trastero != null){
+                ingresoPotencialesTrasteros = ingresoPotencialesTrasteros + trastero.getPrecio();
+            }
+        }
+
+        return ingresoPotencialesTrasteros;
+    }
+
+    public double calculoTrasterosVendidos(){
+        double ingresoTrasterosVendidos = 0;
+
+        for(int puerta = 0;  puerta < numTrasteros ; puerta++){
+            Trastero trastero = trasteros[puerta];
+            if(trastero != null && trastero.estado == Trastero.Estado.VENDIDO ){
+                ingresoTrasterosVendidos = ingresoTrasterosVendidos + trastero.getPrecio();
+            }
+        }
+
+        return ingresoTrasterosVendidos;
+    }
+
+
+
 }
+
